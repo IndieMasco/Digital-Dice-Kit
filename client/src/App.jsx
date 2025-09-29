1a11bbc14bb1149ffebcc25ac501df6771e3dc96
sam <s.clarkyy.1994@gmail.com>
2025-09-29 17:50:28 +0100

    MASSIVE CHANGE: put all the dice into one component, done some styling, added a header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 
-import AllDice from "./components/Dice/AllDice";
+import Header from "./components/header/Header";
+import DiceRoller from "./components/dice/DiceRoller";
 
 export default function App() {
   return (
     <>
-      <AllDice />
+      <Header />
+      <DiceRoller sides={4} />
+      <DiceRoller sides={6} />
+      <DiceRoller sides={8} />
+      <DiceRoller sides={10} />
+      <DiceRoller sides={12} />
+      <DiceRoller sides={20} />
+      <DiceRoller sides={100} />
     </>
   );
 }
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h1 className="header">Digital Dice Kit</h1>
+    </div>
+  );
+}
