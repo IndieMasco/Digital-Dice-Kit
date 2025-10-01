45b11a26433aba7098b65221a8a513213e3bb9c2
sam <s.clarkyy.1994@gmail.com>
2025-10-01 11:07:05 +0100

    updating layout going off feedback

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,19 @@ export default function App() {
   return (
     <>
       <Header />
-      <DiceRoller sides={4} />
-      <DiceRoller sides={6} />
-      <DiceRoller sides={8} />
-      <DiceRoller sides={10} />
-      <DiceRoller sides={12} />
-      <DiceRoller sides={20} />
-      <DiceRoller sides={100} />
+      <div className="app-row">
+        <DiceRoller sides={4} />
+        <DiceRoller sides={6} />
+        <DiceRoller sides={8} />
+      </div>
+      <div className="app-row">
+        <DiceRoller sides={10} />
+        <DiceRoller sides={12} />
+        <DiceRoller sides={20} />
+      </div>
+      <div className="app-row">
+        <DiceRoller sides={100} />
+      </div>
       <Footer />
     </>
   );
